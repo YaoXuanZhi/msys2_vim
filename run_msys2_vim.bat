75e6ff5b494be0557ff728326bf99caf446aa3d9
yaoxuanzhi <yaoxuanzhi@outlook.com>
2021-06-12 10:23:11 +0800

    按照外部传参决定msys2的打开方式

---
+++ run_msys2_vim.bat
@@ -0,0 +1,34 @@
+@echo off
+
+call %~dp0config.bat
+
+set ROOT=%1
+call :removequotes ROOT
+
+pushd "%ROOT%" 2>nul && (call :folder "%ROOT%" & popd) || call :file "%ROOT%"
+GOTO :eof
+
+:file
+call :filename ROOT file_name
+call :dirname ROOT dir_name
+
+set command_args=%msys2_type% -where "%dir_name%"
+call %msys2_shell_bat% %command_args% -outside_exec "vim '%file_name%'"
+GOTO :eof
+
+:folder
+set command_args=%msys2_type% -where "%ROOT%"
+call %msys2_shell_bat% %command_args%
+GOTO :eof
+
+:removequotes
+FOR /F "delims=" %%A IN ('echo %%%1%%') DO set %1=%%~A
+GOTO :eof
+
+:dirname
+FOR /F "delims=" %%A IN ('echo %%%1%%') DO set %2=%%~dpA
+GOTO :eof
+
+:filename
+FOR /F "delims=" %%A IN ('echo %%%1%%') DO set %2=%%~nxA
+GOTO :eof
