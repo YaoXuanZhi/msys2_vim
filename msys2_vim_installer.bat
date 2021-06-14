4346fe8fd5b55f33ec95b6ad4ceef65ad11396e3
yaoxuanzhi <yaoxuanzhi@outlook.com>
2021-06-14 16:16:11 +0800

    添加菜单项图标

--- msys2_vim_installer.bat
+++ msys2_vim_installer.bat
@@ -9,22 +9,28 @@ call "%~dp0create_depends_env.bat"
 ::设置右键菜单项的名称
 set desc="Msys2-Vim"
 ::设置执行脚本
-set runbat="run_msys2_vim.bat"
+set run_bat="run_msys2_vim.bat"
+::设置菜单项图标路径
+set icon_path="%~dp0msys2.ico"
 ::设置子路径名称
 set subname="msys2_vim"
 ::去掉双引号，主要是为了兼容包含了空格的文件名或路径
-call :removequotes runbat
+call :removequotes run_bat
 call :removequotes subname
+call :removequotes icon_path
 
 ::添加注册表信息
 reg add "HKEY_CLASSES_ROOT\Directory\background\shell\%subname%" /v "" /t REG_SZ /d %desc%
-reg add "HKEY_CLASSES_ROOT\Directory\background\shell\%subname%\command" /v ""  /t REG_EXPAND_SZ /d "\"%~dp0%runbat%\" \"%%V\"
+reg add "HKEY_CLASSES_ROOT\Directory\background\shell\%subname%\command" /v ""  /t REG_EXPAND_SZ /d "\"%~dp0%run_bat%\" \"%%V\"
+reg add "HKEY_CLASSES_ROOT\Directory\background\shell\%subname%" /v "Icon" /t REG_EXPAND_SZ /d "%icon_path%"
 
 reg add "HKEY_CURRENT_USER\Software\Classes\*\shell\%subname%" /v "" /t REG_SZ /d %desc%
-reg add "HKEY_CURRENT_USER\Software\Classes\*\shell\%subname%\command" /v "" /t REG_EXPAND_SZ /d "\"%~dp0%runbat%\" \"%%V\"
+reg add "HKEY_CURRENT_USER\Software\Classes\*\shell\%subname%\command" /v "" /t REG_EXPAND_SZ /d "\"%~dp0%run_bat%\" \"%%V\"
+reg add "HKEY_CURRENT_USER\Software\Classes\*\shell\%subname%" /v "Icon" /t REG_EXPAND_SZ /d "%icon_path%
 
 reg add "HKEY_CLASSES_ROOT\Directory\shell\%subname%" /v "" /t REG_SZ /d %desc%
-reg add "HKEY_CLASSES_ROOT\Directory\shell\%subname%\command" /v "" /t REG_EXPAND_SZ /d "\"%~dp0%runbat%\" \"%%V\"
+reg add "HKEY_CLASSES_ROOT\Directory\shell\%subname%\command" /v "" /t REG_EXPAND_SZ /d "\"%~dp0%run_bat%\" \"%%V\"
+reg add "HKEY_CLASSES_ROOT\Directory\shell\%subname%" /v "Icon" /t REG_EXPAND_SZ /d "%icon_path%"
 
 :removequotes
 FOR /F "delims=" %%A IN ('echo %%%1%%') DO set %1=%%~A
