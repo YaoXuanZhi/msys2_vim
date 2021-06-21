fcfcb2bdab35c1f1462adf5fac6349fd04bae5fc
huangguangyao <huangguangyao@123.com>
2021-06-21 17:08:08 +0800

    如果配置已存在，则跳过目录关联

--- create_depends_env.bat
+++ create_depends_env.bat
@@ -1,5 +1,10 @@
 @echo off
 REM 创建目录关联
+set config_path="%~dp0config.bat"
+
+if exist "%~dp0config.bat" (
+    GOTO :eof
+)
 
 echo 将msys2目录粘贴到这里
 set /p msys2_dir=
@@ -7,10 +12,9 @@ set /p msys2_dir=
 call :removequotes msys2_dir
 
 @REM 自动生成config.bat
-set config_path="%~dp0config.bat"
-if not exist %config_path% (
+if not exist "%~dp0config.bat" (
     echo @echo off>> %config_path%
-    echo set msys2_install_dir="%msys2_dir%">> %config_path%
+    echo set msys2_shell_dir="%msys2_dir%">> %config_path%
     echo set msys2_shell_bat="%msys2_dir%\msys2_shell.cmd">> %config_path%
     echo set msys2_type=-mingw64>> %config_path%
     echo set pause_cmd=read -p 'Please press any key after'>> %config_path%
