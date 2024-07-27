bbf0b041bdc76f2264d81a2affc3c8bd4bd4c3f0
YaoXuanZhi <yaoxuanzhi@outlook.com>
2024-07-27 21:57:12 +0800

    添加快速删除命令

---
+++ quick_delete_folder_installer.bat
@@ -0,0 +1,35 @@
+@echo off
+::判断是否已经获取了管理员身份
+Md "%WinDir%\System32\test_permissions" 2>NUL||(Echo 请使用右键管理员身份运行&&Pause >NUL&&Exit)
+Rd "%WinDir%\System32\test_permissions" 2>NUL
+SetLocal EnableDelayedExpansion
+
+::设置右键菜单项的名称
+set desc="Quick-Delete-Folder"
+::设置执行脚本
+set run_bat="run_quick_delete_folder.bat"
+::设置菜单项图标路径
+set icon_path="git.exe"
+::设置子路径名称
+set subname="quick_delete_folder"
+::去掉双引号，主要是为了兼容包含了空格的文件名或路径
+call :removequotes run_bat
+call :removequotes subname
+call :removequotes icon_path
+
+::添加注册表信息
+reg add "HKEY_CLASSES_ROOT\Directory\background\shell\%subname%" /v "" /t REG_SZ /d %desc%
+reg add "HKEY_CLASSES_ROOT\Directory\background\shell\%subname%\command" /v ""  /t REG_EXPAND_SZ /d "\"%~dp0%run_bat%\" \"%%V\"
+reg add "HKEY_CLASSES_ROOT\Directory\background\shell\%subname%" /v "Icon" /t REG_EXPAND_SZ /d "%icon_path%"
+
+reg add "HKEY_CURRENT_USER\Software\Classes\*\shell\%subname%" /v "" /t REG_SZ /d %desc%
+reg add "HKEY_CURRENT_USER\Software\Classes\*\shell\%subname%\command" /v "" /t REG_EXPAND_SZ /d "\"%~dp0%run_bat%\" \"%%V\"
+reg add "HKEY_CURRENT_USER\Software\Classes\*\shell\%subname%" /v "Icon" /t REG_EXPAND_SZ /d "%icon_path%
+
+reg add "HKEY_CLASSES_ROOT\Directory\shell\%subname%" /v "" /t REG_SZ /d %desc%
+reg add "HKEY_CLASSES_ROOT\Directory\shell\%subname%\command" /v "" /t REG_EXPAND_SZ /d "\"%~dp0%run_bat%\" \"%%V\"
+reg add "HKEY_CLASSES_ROOT\Directory\shell\%subname%" /v "Icon" /t REG_EXPAND_SZ /d "%icon_path%"
+
+:removequotes
+FOR /F "delims=" %%A IN ('echo %%%1%%') DO set %1=%%~A
+GOTO :eof
---
+++ run_quick_delete_folder.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set ROOT=%1
+
+cd /d %ROOT%/..
+
+rd /s /q %root%
